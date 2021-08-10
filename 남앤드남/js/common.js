9bf4d80536600873e6a09565b6b515b9cd93b38e
DESKTOP-3LGFNGK\리플렉션 <seodoha@reflexion.co.kr>
2021-08-10 09:46:54 +0900

    font 정리

--- 남앤드남/js/common.js
+++ 남앤드남/js/common.js
@@ -30,15 +30,12 @@ App.Common = (function() {
         init : function() {
             self = this;
             self.common();
-
             self.sectionInit();
             self.sectionScroll();
         },
         common : function() {
-            
         },
         sectionInit: function() {
-            $('main').css({'height' : $(window).height() * $('section').length})
             $('section').each(function(index, item){
                 $(item).css({ 'top' : $(item).index() * $(window).height() });    
             });
@@ -54,40 +51,38 @@ App.Common = (function() {
                 _sec03Top = $('.sec03').offset().top,
                 _sec04Top = $('.sec04').offset().top;
 
-            $(window).on('scroll', function(){
+            $(window).on('scroll', function() {
                 var a = $(window).scrollTop(),
                     c = 0,
                     d = -50,
                     _sec01Value = a * ( d - c ) / _sec01Top + c;
                     
-                    // console.log(a);
+                // console.log(a);
 
-                    _sec01Value > -50 && $mainVi.css('top', _sec01Value + "%");
-                    if ( a > _sec01Top && a < _sec02Top) {
-                        a = a - $(window).height();
-                        var _sec02Value = a * ( d - c ) / _sec01Top + c;
-                        $sec01.css({'position' : 'fixed', 'top' : _sec02Value + "%"});
-                    } else {
-                        $sec01.css({'position' : 'absolute', 'top' : _sec01Top});
-                    }
+                _sec01Value > -50 && $mainVi.css('top', _sec01Value + "%");
+                if ( a > _sec01Top && a < _sec02Top) {
+                    a = a - $(window).height();
+                    var _sec02Value = a * ( d - c ) / _sec01Top + c;
+                    $sec01.css({'position' : 'fixed', 'top' : _sec02Value + "%"});
+                } else {
+                    $sec01.css({'position' : 'absolute', 'top' : _sec01Top});
+                }
 
-                    if ( a > _sec02Top && a < _sec03Top ) {
-                        a = a - $(window).height() * 2;
-                        var _sec03Value = a * ( d - c ) / _sec01Top + c;
-                        $sec02.css({'position' : 'fixed', 'top' : _sec03Value + "%"});
-                    } else {
-                        $sec02.css({'position' : 'absolute', 'top' : _sec02Top});
-                    }
+                if ( a > _sec02Top && a < _sec03Top ) {
+                    a = a - $(window).height() * 2;
+                    var _sec03Value = a * ( d - c ) / _sec01Top + c;
+                    $sec02.css({'position' : 'fixed', 'top' : _sec03Value + "%"});
+                } else {
+                    $sec02.css({'position' : 'absolute', 'top' : _sec02Top});
+                }
 
-                    if ( a > _sec03Top && a < _sec04Top ) {
-                        a = a - $(window).height() * 3;
-                        var _sec04Value = a * ( d - c ) / _sec01Top + c;
-                        $sec03.css({'position' : 'fixed', 'top' : _sec04Value + "%"});
-                    } else {
-                        $sec03.css({'position' : 'absolute', 'top' : _sec03Top});
-                    }
-
-        
+                if ( a > _sec03Top && a < _sec04Top ) {
+                    a = a - $(window).height() * 3;
+                    var _sec04Value = a * ( d - c ) / _sec01Top + c;
+                    $sec03.css({'position' : 'fixed', 'top' : _sec04Value + "%"});
+                } else {
+                    $sec03.css({'position' : 'absolute', 'top' : _sec03Top});
+                }
             });
         },
     }
