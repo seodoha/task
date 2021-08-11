2b5e411db84f258c0c15c8b20a32b025a0eb69e3
DESKTOP-3LGFNGK\리플렉션 <seodoha@reflexion.co.kr>
2021-08-11 17:59:20 +0900

    update

--- 남앤드남/js/main.js
+++ 남앤드남/js/main.js
@@ -4,6 +4,8 @@
 
 $(document).ready(function(){
     App.Main.init();
+
+    
 });
 
 //---------------------------------------------------------------------------------------
@@ -18,7 +20,7 @@ App.Main = (function () {
             self.sectionInit();
             self.sectionScroll();
             self.scrollAni();
-
+            
             new Swiper('.mainViSlide', {
                 slidesPerView: 1,
                 effect: "fade",
@@ -30,7 +32,11 @@ App.Main = (function () {
         },
         sectionInit: function() {
             $('section').each(function(index, item){
-                $(item).css({ 'top' : $(item).index() * $(window).height() });    
+                var height = $(item).index() * $(window).height();
+                if ( index == 3 ) {
+                    height = height + 600;
+                }
+                $(item).css({ 'top' :  height});
             });
         },
         sectionScroll: function() {
@@ -39,13 +45,9 @@ App.Main = (function () {
                 $sec01 = $('.sec01'),
                 $sec02 = $('.sec02'),
                 $sec03 = $('.sec03'),
-                $sec04 = $('.sec04'),
-                $sec05 = $('.sec05'),
                 _sec01Top = $sec01.offset().top,
                 _sec02Top = $sec02.offset().top,
-                _sec03Top = $sec03.offset().top,
-                _sec04Top = $sec04.offset().top,
-                _sec05Top = $sec05.offset().top;
+                _sec03Top = $sec03.offset().top;
 
             $(window).on('scroll', function() {
                 var a = $(window).scrollTop(),
@@ -59,58 +61,149 @@ App.Main = (function () {
                 if ( a > _sec01Top && a < _sec02Top) {
                     a = a - $(window).height();
                     var _sec02Value = a * ( d - c ) / _sec01Top + c;
-                    $sec01.css({'position' : 'fixed', 'top' : _sec02Value + "%"});
                     $header.addClass('index01');
+                    $sec01.css({'position' : 'fixed', 'top' : _sec02Value + "%"});
                 } else {
-                    $sec01.css({'position' : 'absolute', 'top' : _sec01Top});
                     $header.removeClass('index01');
+                    $sec01.css({'position' : 'absolute', 'top' : _sec01Top});
                 }
 
                 if ( a > _sec02Top && a < _sec03Top ) {
-                    a = a - $(window).height() * 2;
+                    $sec02.css({'position' : 'fixed', 'top' : 0});
+                    a = a - $(window).height() * 2 - 600;
                     var _sec03Value = a * ( d - c ) / _sec01Top + c;
-                    $sec02.css({'position' : 'fixed', 'top' : _sec03Value + "%"});
+                    
+                    if ( $(window).scrollTop() > _sec02Top + 600 ) {
+                        $sec02.css({'top' : _sec03Value + "%"});
+                    }
+
                 } else {
                     $sec02.css({'position' : 'absolute', 'top' : _sec02Top});
                 }
 
-                if ( a > _sec03Top && a < _sec04Top ) {
+                if ( a > _sec03Top ) {
                     a = a - $(window).height() * 3;
                     var _sec04Value = a * ( d - c ) / _sec01Top + c;
                     $sec03.css({'position' : 'fixed', 'top' : _sec04Value + "%"});
                 } else {
                     $sec03.css({'position' : 'absolute', 'top' : _sec03Top});
-                }
-
-                if ( a > _sec04Top && a < _sec05Top ) {
-                    a = a - $(window).height() * 4;
-                    var _sec05Value = a * ( d - c ) / _sec01Top + c;
-                    $sec04.css({'position' : 'fixed', 'top' : _sec05Value + "%"});
-                } else {
-                    $sec04.css({'position' : 'absolute', 'top' : _sec04Top});
-                }
+                }                
             });
         },
         scrollAni: function() {
             var $sec01 = $('.sec01'),
                 $sec02 = $('.sec02'),
-                $sec03 = $('.sec03'),
-                $sec04 = $('.sec04'),
-                $sec05 = $('.sec05'),
-                _sec01Top = $sec01.offset().top - 500,
-                _sec02Top = $sec02.offset().top - 500,
-                _sec03Top = $sec03.offset().top - 500,
-                _sec04Top = $sec04.offset().top - 500,
-                _sec05Top = $sec05.offset().top - 500;
-
-            $(window).on('scroll', function() {
+                $secInfo = $('.sec02 .sec02Info'),
+                _sec01Top = $sec01.offset().top - 400,
+                _sec02Top = $sec02.offset().top - 400,
+                _sec02Posi01 = $sec02.offset().top + 100,
+                _sec02Posi02 = $sec02.offset().top + 300,
+                _sec02Posi03 = $sec02.offset().top + 500;
+            
+            $(window).on('scroll', function(e) {
                 var scTop = $(window).scrollTop();
 
                 scTop > _sec01Top && $sec01.addClass('ani');
-                scTop > _sec02Top && $sec02.addClass('ani');
-                scTop > _sec03Top && $sec03.addClass('ani');
-                scTop > _sec04Top && $sec04.addClass('ani');
-                scTop > _sec05Top && $sec05.addClass('ani');
+                if ( scTop > _sec02Top ) {
+                    $sec02.addClass('ani');
+                    
+                    if ( scTop < _sec02Posi01 ) {
+                        if ( !$secInfo.eq(0).hasClass('active') ) {
+                            $secInfo.eq(0).addClass('active').css('top', '-30%').stop().animate({
+                                'top':'0', 
+                                'opacity': '1',
+                                'z-index': '0'
+                            }, 200);
+                        }
+                        if ( $secInfo.eq(1).hasClass('active') ) {
+                            $secInfo.eq(1).removeClass('active').stop().animate({
+                                'top':'80%', 
+                                'opacity': '0',
+                                'z-index': '-1'
+                            }, 200);
+                        }
+                    }
+                        
+                    if ( scTop > _sec02Posi01 && scTop < _sec02Posi02 ) {
+                        if ( $secInfo.eq(0).hasClass('active') ) {
+                            $secInfo.eq(0).removeClass('active').stop().animate({
+                                'top':'-80%',
+                                'opacity':'0',
+                                'z-index':'-1'
+                            }, 200);
+                        }
+                        if ( !$secInfo.eq(1).hasClass('active') ) {
+                            $secInfo.eq(1).addClass('active').css('top', '80%').stop().animate({
+                                'top':'0', 
+                                'opacity': '1',
+                                'z-index': '0'
+                            }, 200);
+                            $('.odometer').text(0);
+                            setTimeout(function(){
+                                $secInfo.eq(1).find('.odometer').text(120);
+                            }, 1000);
+                        }
+                        if ( $secInfo.eq(2).hasClass('active') ) {
+                            $secInfo.eq(2).removeClass('active').stop().animate({
+                                'top':'80%',
+                                'opacity':'0',
+                                'z-index':'-1'
+                            }, 200);
+                        }
+                    }
+
+                    if ( scTop > _sec02Posi02 && scTop < _sec02Posi03 ) {
+                        if ( $secInfo.eq(1).hasClass('active') ) {
+                            $secInfo.eq(1).removeClass('active').stop().animate({
+                                'top':'-80%',
+                                'opacity':'0',
+                                'z-index':'-1'
+                            }, 200);
+                        }
+                        if ( !$secInfo.eq(2).hasClass('active') ) {
+                            $secInfo.eq(2).addClass('active').css('top', '80%').stop().animate({
+                                'top':'0', 
+                                'opacity': '1',
+                                'z-index': '0'
+                            }, 200);
+                            $('.odometer02').text(0);
+                            setTimeout(function(){
+                                $secInfo.eq(2).find('.odometer02').text(2000);
+                            }, 1000);
+                        }
+                        if ( $secInfo.eq(3).hasClass('active') ) {
+                            $secInfo.eq(3).removeClass('active').stop().animate({
+                                'top':'80%',
+                                'opacity':'0',
+                                'z-index':'-1'
+                            }, 200);
+                        }
+                    }
+
+                    if ( scTop > _sec02Posi03 ) {
+                        if ( $secInfo.eq(2).hasClass('active') ) {
+                            $secInfo.eq(2).removeClass('active').stop().animate({
+                                'top':'-80%',
+                                'opacity':'0',
+                                'z-index':'-1'
+                            }, 200);
+                        }
+                        if ( !$secInfo.eq(3).hasClass('active') ) {
+                            $secInfo.eq(3).addClass('active').css('top', '80%').stop().animate({
+                                'top':'0', 
+                                'opacity': '1',
+                                'z-index': '0'
+                            }, 200);
+                            $('.odometer03').text(0);
+                            setTimeout(function(){
+                                $secInfo.eq(3).find('.odometer03').text(40);
+                            }, 1000);
+                        }
+                    }
+    
+                }
+
+
             });
         },
     }
